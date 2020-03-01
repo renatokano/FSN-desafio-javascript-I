64f62e1b4d00cb708fdac2f012f5711e99959992
renatokano <renatokano16@gmail.com>
2020-02-29 23:59:37 -0300

    Modularizando funções auxiliares

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Base a ser utilizada
 const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
 
-// implementação
-// helpers
-let exibirBarra = ()=>console.log("........................................................");
+const {exibirBarra} = require("./escolarHelper");
 
 // feature/listar-alunos
 let listarAlunos = (baseDeAlunos) => {
